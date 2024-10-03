aaa1447741a4ad0d15d406055ba43defc27cc8c8
Ray Thomas <rthomas5678@gmail.com>
2024-10-02 17:13:36 -0700

    added user and post models and connencted db

--- server/server.js
+++ server/server.js
@@ -4,5 +4,19 @@ const cors = require('cors')
 const dotenv = require('dotenv').config()
 
 const app = express()
-
 app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Connected to Database, Listening on PORT ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    });
